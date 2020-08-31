75bda29754fa9c29603b9e6dcd01f7516dfa92dc
pratikshad16 <dhagepratiksha16@gmail.com>
2020-09-01 01:30:43 +0530

    Refactored: All the three to one generic method

--- src/main/java/com/bridgelabz/genericsmaximumproblem/service/MaximumNumber.java
+++ src/main/java/com/bridgelabz/genericsmaximumproblem/service/MaximumNumber.java
@@ -1,33 +1,13 @@
 package com.bridgelabz.genericsmaximumproblem.service;
 
 public class MaximumNumber {
-
-    public Integer maximumIntegerValue(Integer firstNumber,Integer secondNumber,Integer thirdNumber)
-    {
-        if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNumber) > 0)
-            return firstNumber;
-        else if (secondNumber.compareTo(thirdNumber) > 0 && secondNumber.compareTo(firstNumber) > 0)
-            return secondNumber;
-        else
-            return thirdNumber;
-    }
-    public Float maximumFloatValue(Float firstNumber,Float secondNumber,Float thirdNumber)
-    {
-        if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNumber) > 0)
-            return firstNumber;
-        else if (secondNumber.compareTo(thirdNumber) > 0 && secondNumber.compareTo(firstNumber) > 0)
-            return secondNumber;
-        else
-            return thirdNumber;
-    }
-    public String maximumStringValue(String firstString, String secondString, String thirdString)
-    {
-        if (firstString.length() > secondString.length()  && firstString.length() > thirdString.length())
-            return firstString;
-        else if (secondString.length() > thirdString.length() && secondString.length() > firstString.length())
-            return secondString;
-        else
-            return thirdString;
+    public <E extends Comparable> E maximumValue(E[] array) {
+        E max = array[0];
+        for (int element = 0; element < array.length; element++) {
+            if (array[element].compareTo(max) > 0)
+                max = array[element];
+        }
+        return max;
     }
 
 }
--- src/test/java/com/bridgelabz/genericsmaximumproblem/MaximumNumberTest.java
+++ src/test/java/com/bridgelabz/genericsmaximumproblem/MaximumNumberTest.java
@@ -15,49 +15,49 @@ public class MaximumNumberTest {
     }
     @Test
     public void givenMaxIntNumber_whenInFirstPosition_shouldReturnSame() {
-        Integer maximum = maximumValue.maximumIntegerValue(3,2,1);
+        Integer maximum = maximumValue.maximumValue(new Integer[]{3,1,2});
         Assert.assertEquals((Integer)3,maximum);
     }
 
     @Test
     public void givenMaxIntNumber_whenInSecondPosition_shouldReturnSame() {
-        Integer maximum = maximumValue.maximumIntegerValue(2,3,1);
+        Integer maximum = maximumValue.maximumValue(new Integer[]{1,3,2});
         Assert.assertEquals((Integer)3,maximum);
     }
 
     @Test
     public void givenMaxIntNumber_whenInThirdPosition_shouldReturnSame() {
-        Integer maximum = maximumValue.maximumIntegerValue(2,1,3);
+        Integer maximum = maximumValue.maximumValue(new Integer[]{2,1,3});
         Assert.assertEquals((Integer)3,maximum);
     }
     @Test
     public void givenMaxFloatNumber_whenInFirstPosition_shouldReturnSame() {
-        Float maximum = maximumValue.maximumFloatValue(2.2f,1.3f,1.1f);
+        Float maximum = maximumValue.maximumValue(new Float[]{2.2f,1.2f,1.1f});
         Assert.assertEquals((Float)2.2f,maximum);
     }
     @Test
     public void givenMaxFloatNumber_whenInSecondPosition_shouldReturnSame() {
-        Float maximum = maximumValue.maximumFloatValue(1.3f,2.2f,1.1f);
+        Float maximum = maximumValue.maximumValue(new Float[]{1.2f,2.2f,1.1f});
         Assert.assertEquals((Float)2.2f,maximum);
     }
     @Test
     public void givenMaxFloatNumber_whenInThirdPosition_shouldReturnSame() {
-        Float maximum = maximumValue.maximumFloatValue(1.3f,1.1f,2.2f);
+        Float maximum = maximumValue.maximumValue(new Float[]{1.1f,1.2f,2.2f});
         Assert.assertEquals((Float)2.2f,maximum);
     }
     @Test
     public void givenMaxStringValue_whenInFirstPosition_shouldReturnSame() {
-        String maximum = maximumValue.maximumStringValue("banana","peach","abc");
-        Assert.assertEquals((String)"banana",maximum);
+        String maximum = maximumValue.maximumValue(new String[]{"lmnop","bcde","abc"});
+        Assert.assertEquals((String)"lmnop",maximum);
     }
     @Test
     public void givenMaxStringValue_whenInSecondPosition_shouldReturnSame() {
-        String maximum = maximumValue.maximumStringValue("peach","banana","abc");
-        Assert.assertEquals((String)"banana",maximum);
+        String maximum = maximumValue.maximumValue(new String[]{"abc","lmnop","bcde"});
+        Assert.assertEquals((String)"lmnop",maximum);
     }
     @Test
     public void givenMaxStringValue_whenInThirdPosition_shouldReturnSame() {
-        String maximum = maximumValue.maximumStringValue("peach","abc","banana");
-        Assert.assertEquals((String)"banana",maximum);
+        String maximum = maximumValue.maximumValue(new String[]{"abc","bcde","lmnop"});
+        Assert.assertEquals((String)"lmnop",maximum);
     }
 }
